682595e746ea0f47105f6c3e8ef166a04b68641c
link5669 <milesacq@icloud.com>
2021-03-28 11:48:58 -0400

    faked an ending

--- LetsStudy/main/views.py
+++ LetsStudy/main/views.py
@@ -47,19 +47,19 @@ def userHome(request):
     return render(request,'main/userHome.html')
 
 def loginPage(request):
-    if request.method == 'POST':
-        username =request.POST.get('username')
-        password =request.POST.get('password')
-
-        user = authenticate(request, username=username, password=password)
-        
-        if user is not None:
-            login(request,username)
-            return render(request, 'userHome/')
-        else:
-            messages.info(request, 'Username or password is incorrect')
+    # if request.method == 'POST':
+    #     username =request.POST.get('username')
+    #     password =request.POST.get('password')
+    #
+    #     user = authenticate(request, username=username, password=password)
+    #
+    #     if user is not None:
+    #         login(request,username)
+    #         return render(request, 'userHome/')
+    #     else:
+    #         messages.info(request, 'Username or password is incorrect')
 
     context={}
-    return render(request,'main/login.html',context)  
+    return render(request,'main/login.html',context)
    
 
