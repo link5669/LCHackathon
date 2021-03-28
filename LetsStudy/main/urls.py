6cd601e5223178d9360fb2b888c8f02039710d9a
link5669 <milesacq@icloud.com>
2021-03-27 22:29:34 -0400

    added subpages for classes

--- LetsStudy/main/urls.py
+++ LetsStudy/main/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
     path('',views.index),
     path('register/',views.registerPage, name ="register"),
     path('login/', views.loginPage, name = "login"),
-
+    path('introcs/',views.registerPage, name ="introcs"),
+    path('expos/',views.registerPage, name ="expos"),
+    path('DS/',views.registerPage, name ="DS"),
+    path('physics/',views.registerPage, name ="physics"),
+    path('chemistry/',views.registerPage, name ="chemistry"),
+    path('musictheory/',views.registerPage, name ="musictheory"),
 ]
